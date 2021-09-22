c8641b539e658901b5b9efd5083c9ca6979f1f30
meroveDev <damien.lhaurado@my-digital-school.org>
2021-09-22 14:17:19 +0200

    local storage OK

--- app.js
+++ app.js
@@ -2,34 +2,40 @@
 // {
 
 // };
-const stat = {hungry: 100, stamina: 100, fun : 10};
+let stat = {hungry: 3, stamina: 100, fun : 100};
 const divs = {
     hungry : document.getElementById("stat_hungry"),
     stamina : document.getElementById("stat_stamina"),
     fun : document.getElementById("stat_fun"),
 }
-var burned = false;
-var stored = false;
-var timer = 1000;
+//var status = {burned = false, stored = false};
+let burned = false;
+let stored = false;
+let timer = 1000;
+let berries = 5;
+let pokedollar = 50;
 
 const interval = setInterval(() =>{
     for(let i in stat) {
-        console.log(burned)
         if(i == "stamina" && stored && stat[i] + 5 < 100) {
             stat[i] += 5;
         } else if(i == "stamina" && burned) {
             stat[i] -= 5;
+        } else if (stored) {
+            stat[i] -= 5;
         } else {
             stat[i]--;
         }
-        divs[i].innerHTML = stat[i];
+        divs[i].innerHTML = stat[i] + " %";
     }
+    document.getElementById("berries").innerHTML = berries + " baies"
     verifGameOver();
     }, timer)
 
 function actionTama(param) {
-    if(param == "hungry" && stat.hungry + 10 < 100) {
+    if(param == "hungry" && stat.hungry + 10 < 100 && berries > 0) {
         stat.hungry += 10;
+        berries--;
     } 
     if(param == "fun" && stat.fun + 10 < 100) {
         stat.fun += 10;
@@ -46,27 +52,53 @@ function actionTama(param) {
     //     document.getElementById("status").classList.add("d-none");
     // } 
 }
-function verifGameOver() {
-    if(stat.hungry == 0 || stat.hungry < 0 ||
-        stat.stamina == 0 || stat.stamina < 0 ||
-        stat.fun == 0 || stat.fun < 0) {
-            document.getElementById("mainFrame").innerHTML = "GAME OVER"
-        } 
-}
+
 function closeLight(){
     if (document.querySelector("#mainFrame").classList.contains('night')) {
         stored = false;
         document.getElementById("mainFrame").classList.remove("night");
         document.getElementById("buttonStockPc").innerHTML = "Emmener au centre pokémon";
+
+        document.getElementById("buttonHungry").disabled = false;
+        document.getElementById("buttonFun").disabled = false;
+       
+        document.getElementById("buttonHungry").setAttribute("onclick", "actionTama('hungry')")
+        document.getElementById("buttonFun").setAttribute("onclick", "actionTama('fun')")
     } else {
         stored = true;
         burned = false;
         document.getElementById("status").classList.add("d-none");
         document.getElementById("mainFrame").classList.add("night");
         document.getElementById("buttonStockPc").innerHTML = "Retirer du centre pokémon";
+
+        document.getElementById("buttonHungry").disabled = true;
+        document.getElementById("buttonFun").disabled = true;
+        
+        document.getElementById("buttonHungry").setAttribute("onclick", "")
+        document.getElementById("buttonFun").setAttribute("onclick", "")
     }
 }
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
+}
+// JSON.stringify(stat)
+// JSON.parse()
+function save() {
+    localStorage.setItem('STATS', JSON.stringify(stat));
+}
+
+function getSave(){
+    let data = localStorage.getItem('STATS')
+    console.log(JSON.parse(data))
+    stat = JSON.parse(data);
+}
+
+function verifGameOver() {
+    if(stat.hungry == 0 || stat.hungry < 0 ||
+        stat.stamina == 0 || stat.stamina < 0 ||
+        stat.fun == 0 || stat.fun < 0) {
+            document.getElementById("gameOver").innerHTML = "GAME OVER"
+            clearInterval(interval);
+        } 
 }
